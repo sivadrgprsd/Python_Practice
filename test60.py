299b227f95b7ba66dea7fdcc9af9cad5bf409434
siva <sivadrgprsd@gmail.com>
2024-08-05 09:17:58 +0530

    test

---
+++ test60.py
@@ -0,0 +1,19 @@
+import re
+str= ["Hello","DevOps","Python"]
+
+result= str[0] + " " + str[1] + " " + str[2]
+
+search=re.search("DevOps1", result)
+if search:
+    print("Pattern found:", search.group())
+else:
+    print("Pattern not found")    
+
+print(len(result))
+print(result.upper())
+print(result.lower())
+print(result.replace("DevOps","Pega"))  
+print(result.split())
+print("Modified text:", re.sub(str[2], str[0], result))
+print("split_result:",re.split(str[1], result))
+
