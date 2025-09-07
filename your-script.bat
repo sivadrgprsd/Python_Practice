267c447bda1a4e9a38b770125f1180fd35ecbff5
sivadrgprsd <161949686+sivadrgprsd@users.noreply.github.com>
2025-09-07 21:48:45 +0530

    Update and rename your-script.sh to your-script.bat

---
+++ your-script.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Hostname: %COMPUTERNAME%
+echo Date: %DATE% %TIME%
+echo Current User: %USERNAME%
+echo Current Directory: %CD%
+echo.
+echo Hello, %USERNAME%! Welcome to your script demo.
